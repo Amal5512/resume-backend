4577e4eea02c0c6f2165b78fd2215d156fe06acd
Amal5512 <amalkumaranil1999@gmail.com>
2024-02-09 16:11:56 +0530

    security

--- controller/userrouter.js
+++ controller/userrouter.js
@@ -1,14 +1,30 @@
 const express=require("express")
 const usermodel = require("../models/usermodel")
 const router=express.Router()
+const bcrypt=require("bcryptjs")
 
 
-router.post("/signup",async(req,res)=>{
-    let data=req.body
-    let post=usermodel(data)
-    let result=await post.save()
-    res.json({
-        status:"success"
-    })
+hashPaswordGenerator =async(pass)=>{
+    const salt = await bcrypt.genSalt(10)
+    return bcrypt.hash(pass,salt)
+}
+
+router.post("/signup", async(req,res)=>{
+    let {data} = {"data":req.body}
+      let password =data.password
+        hashPaswordGenerator(password).then(
+            (hashedPassword)=>{
+                console.log(hashedPassword)
+                data.password=hashedPassword
+                console.log(data)
+               
+                 let user =new usermodel(data)
+                let result =user.save()
+               
+                res.json({status:"success"})
+            }
+        )
+
+   
 })
 module.exports=router
